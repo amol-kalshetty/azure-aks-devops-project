0f1927956cf327a77671847da1a15a76ca410641
amol kalshetty <amolkalshetty5920@gmail.com>
2026-08-30 13:51:13 +0530

    updated v4

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure AKS DevOps Project - Version 3!"
+    return "Hello from Azure AKS DevOps Project - Version 4!"
 
 @app.route("/health")
 def health():
